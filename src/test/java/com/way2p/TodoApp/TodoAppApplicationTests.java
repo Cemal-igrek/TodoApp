baaac0bb85ac4c0328b35f259adcefbe92704ce9
Cemal <c.igrek63@hotmail.com>
2025-03-17 04:15:21 +0300

    Initial commit

---
+++ src/test/java/com/way2p/TodoApp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.way2p.TodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
