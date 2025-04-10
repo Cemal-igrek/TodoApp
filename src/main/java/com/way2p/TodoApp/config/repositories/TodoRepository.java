8aa14e680cc158475b4f277cb8ca990d829c2a9c
Cemal <c.igrek63@hotmail.com>
2025-04-10 16:50:03 +0300

    fix

--- src/main/java/com/way2p/TodoApp/config/repositories/TodoRepository.java
+++ src/main/java/com/way2p/TodoApp/config/repositories/TodoRepository.java
@@ -2,7 +2,14 @@ package com.way2p.TodoApp.config.repositories;
 
 import com.way2p.TodoApp.config.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TodoRepository extends JpaRepository<Todo,Integer> {
 
+    @Query(value = "SELECT * FROM Todo", nativeQuery = true)
+    List<Todo> findAll();
 }
